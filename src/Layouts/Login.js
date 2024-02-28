567d4fd1497c4f2ebc165a9691237e69bd3cf32c
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2024-02-28 12:00:26 +0800

    login modal updated

--- src/Layouts/Login.js
+++ src/Layouts/Login.js
@@ -17,6 +17,7 @@ import FormControl from "@mui/material/FormControl";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import IconButton from "@mui/material/IconButton";
+import login_modal_banner from "../Assets/login-modal-banner.webp";
 
 //API
 import authService from "../Services/auth.service";
@@ -75,156 +76,159 @@ export default function Login() {
   };
 
   return (
-    <div className=" bg-white px-5 py-8 rounded-lg flex flex-col justify-center items-center gap-5">
-      <div className="header-container flex flex-col justify-center items-center mb-5">
-        <Typography
-          component="h1"
-          variant="h5"
-          sx={{
-            fontFamily: "Poppins, sans-serif",
-            fontWeight: "600",
-            fontSize: "2rem",
-          }}
-        >
-          Platform Login
-        </Typography>
-        <Typography
-          component="h1"
-          variant="h5"
-          sx={{
-            fontFamily: "Poppins, sans-serif",
-            fontWeight: "300",
-            fontSize: "1rem",
-          }}
-        >
-          Welcome!
-        </Typography>
+    <div className="flex rounded-lg bg-white">
+      <div>
+        <img src={login_modal_banner} className="h-full rounded-s-lg" />
       </div>
-      <Box
-        className="input-container flex flex-col justify-center items-center  w-full "
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-        noValidate
-      >
-        <div className="auth-container flex flex-col w-full ">
-          <div className="flex flex-col gap-5 justify-between items-center">
-            <Controller
-              name="username"
-              control={control}
-              defaultValue=""
-              rules={{
-                required: {
-                  value: true,
-                  message: "username is required",
-                },
-              }}
-              render={({ field, fieldState: { error } }) => (
-                <TextField
-                  {...field}
-                  label="Username"
-                  variant="outlined"
-                  fullWidth
-                  inputProps={{
-                    style: {
-                      fontFamily: "Poppins, sans-serif",
-                      fontSize: "0.8rem",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      fontFamily: "Poppins, sans-serif",
-                      fontSize: "0.9rem",
-                    },
-                  }}
-                  error={error !== undefined}
-                  helperText={error ? error.message : ""}
-                />
-              )}
-            />
-            <Controller
-              name="password"
-              control={control}
-              defaultValue=""
-              rules={{
-                required: "Password is required",
-                minLength: {
-                  value: 8,
-                  message: "Password must be at least 8 characters long",
-                },
-              }}
-              render={({ field, fieldState: { error } }) => (
-                <FormControl variant="outlined" fullWidth>
-                  <InputLabel htmlFor="password">Password</InputLabel>
-                  <OutlinedInput
-                    id="password"
+      <div className="  px-5 py-8 flex flex-col justify-center items-center gap-5 w-full">
+        <div className="header-container flex flex-col justify-center items-center mb-5">
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{
+              fontFamily: "Poppins, sans-serif",
+              fontWeight: "600",
+              fontSize: "2rem",
+            }}
+          >
+            Platform Login
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{
+              fontFamily: "Poppins, sans-serif",
+              fontWeight: "300",
+              fontSize: "1rem",
+            }}
+          >
+            Welcome!
+          </Typography>
+        </div>
+        <Box
+          className="input-container flex flex-col justify-center items-center  w-full "
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <div className="auth-container flex flex-col w-full ">
+            <div className="flex flex-col gap-5 justify-between items-center">
+              <Controller
+                name="username"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: {
+                    value: true,
+                    message: "username is required",
+                  },
+                }}
+                render={({ field, fieldState: { error } }) => (
+                  <TextField
                     {...field}
-                    type={showPassword ? "text" : "password"}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    label="Password"
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                    inputProps={{
+                      style: {
+                        fontFamily: "Poppins, sans-serif",
+                        fontSize: "0.8rem",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        fontFamily: "Poppins, sans-serif",
+                        fontSize: "0.9rem",
+                      },
+                    }}
                     error={error !== undefined}
                     helperText={error ? error.message : ""}
                   />
-                </FormControl>
-              )}
-            />
+                )}
+              />
+              <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters long",
+                  },
+                }}
+                render={({ field, fieldState: { error } }) => (
+                  <FormControl variant="outlined" fullWidth>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                      id="password"
+                      {...field}
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                      error={error !== undefined}
+                      helperText={error ? error.message : ""}
+                    />
+                  </FormControl>
+                )}
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label={
+                  <Typography
+                    sx={{ fontSize: 14, fontFamily: "Poppins, sans serif" }}
+                  >
+                    Remember Me
+                  </Typography>
+                }
+              />
+              <Link
+                href="#"
+                variant="body2"
+                className="font-[Poppins] no-underline text-xs"
+              >
+                Forgot password?
+              </Link>
+            </div>
           </div>
-          <div className="flex justify-between items-center">
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label={
-                <Typography
-                  sx={{ fontSize: 14, fontFamily: "Poppins, sans serif" }}
-                >
-                  Remember Me
-                </Typography>
-              }
-            />
+          <div className="button-container flex flex-col justify-center items-center w-full ">
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className="bg-black my-3 py-3 bg-gradient-to-r from-indigo-500 to-pink-500"
+            >
+              Sign In
+            </Button>
             <Link
-              href="#"
+              href="/#"
               variant="body2"
               className="font-[Poppins] no-underline text-xs"
+              onClick={() => {
+                dispatch(handleRegisterOpen());
+                dispatch(handleLoginClose());
+              }}
             >
-              Forgot password?
+              Don't have an account yet? <span>Join Now!</span>
             </Link>
           </div>
+        </Box>
+        <div className="login-footer border-2 border-red-600 h-36 rounded-lg w-full  flex justify-center items-center">
+          login footer
         </div>
-
-        <div className="button-container flex flex-col justify-center items-center w-full ">
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="bg-black my-3 py-3 bg-gradient-to-r from-indigo-500 to-pink-500"
-          >
-            Sign In
-          </Button>
-          <Link
-            href="/#"
-            variant="body2"
-            className="font-[Poppins] no-underline text-xs"
-            onClick={() => {
-              dispatch(handleRegisterOpen());
-              dispatch(handleLoginClose());
-            }}
-          >
-            Don't have an account yet? <span>Join Now!</span>
-          </Link>
-        </div>
-      </Box>
-
-      <div className="login-footer border-2 border-red-600 h-36 rounded-lg w-full  flex justify-center items-center">
-        login footer
       </div>
     </div>
   );
